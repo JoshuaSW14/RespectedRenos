35373d4b12006afd08b2ad8c2b819fe0e6365b02
Joshua <joshua.symons-webb@mohawkcollege.ca>
2023-01-11 12:21:55 -0500

    testing seo optimization

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'Respected Renos',
+    version: 'Angular 15.0.3',
+    bootstrap: 'Bootstrap 5.2.3',
+    fontawesome: 'Font Awesome 6.2.1',
+  }
+};
