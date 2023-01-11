93eb6347ebb0a9131761217cd28ab1b3ead66b55
Joshua <joshua.symons-webb@mohawkcollege.ca>
2023-01-11 12:27:45 -0500

    updated prod environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: false
+  production: false,
+  application:
+  {
+    name: 'Respected Renos',
+    version: 'Angular 15.0.3',
+    bootstrap: 'Bootstrap 5.2.3',
+    fontawesome: 'Font Awesome 6.2.1',
+  }
 };
